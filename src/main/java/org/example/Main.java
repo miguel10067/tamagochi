07e39852451f1dae92c5442316e910a1a461f523
CAMPOS ARANDA <migcamara@alu.edu.gva.es>
2026-01-08 16:16:33 +0000

    MENU PRINCIPAL

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,36 @@
 package org.example;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        int o;
+        do {
+            o = mostrarMenu(lector);
 
-        System.out.println("Hola Git");
+            System.out.println("Has elegido la opción: " + o);
 
+        } while (o != 0);
+    }
+
+    private static int mostrarMenu(Scanner lector) {
+        int o;
+        do {
+            System.out.println("\n--- MENU TAMAGOTCHI ---");
+            System.out.println("1. Estado");
+            System.out.println("2. Comer");
+            System.out.println("3. Jugar");
+            System.out.println("4. Dormir");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opcion del menu: ");
+
+            o = lector.nextInt();
+
+            if (o < 0 || o > 4) {
+                System.out.println("La opción no es válida, intentalo de nuevo.");
+            }
+        } while (o < 0 || o > 4);
+
+        return o;
     }
 }
+
