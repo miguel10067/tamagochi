caf710390a5f4ea66bfc359135f680286052ee4b
CAMPOS ARANDA <migcamara@alu.edu.gva.es>
2026-01-08 16:27:41 +0000

    MostrarEstado y variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,37 @@
 package org.example;
 import java.util.Scanner;
 public class Main {
+    static int saciedad = 6;
+    static int energia = 6;
+    static int diversion = 6;
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         int o;
         do {
             o = mostrarMenu(lector);
 
-            System.out.println("Has elegido la opción: " + o);
-
+            if (o == 1) {
+                mostrarEstado();
+            }
         } while (o != 0);
     }
+    private static void mostrarEstado() {
+        System.out.println("\n--- ESTADO ACTUAL ---");
+        System.out.println("Saciedad: " + saciedad + " | Energía: " + energia + " | Diversión: " + diversion);
 
+        if (saciedad <= 0 || energia <= 0 || diversion <= 0) {
+            System.out.println("(x_x) Game Over");
+            System.exit(0);
+        } else if (diversion <= 4) {
+            System.out.println("(-,-) Estoy aburrido");
+        } else if (energia <= 4) {
+            System.out.println("(-_-) Zzz");
+        } else if (saciedad <= 4) {
+            System.out.println("(·.·) Tengo hambre!");
+        } else {
+            System.out.println("(^.^) ¡Estoy feliz!");
+        }
+    }
     private static int mostrarMenu(Scanner lector) {
         int o;
         do {
